2bfd15cae5e014037bf2ab9a474bcf5548a32253
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-06-18 18:19:50 +0300

    work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('feedback-form textarea');
+const formEl = document.querySelector('feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+populareForm = {};
+
+function onFormSubmit(ev) {
+    ev.preventDefaunt();
+    
+    if (textareaEl.value === '' || inputEl.value === '') {
+        return alert('Заповніть всі поля');
+
+        console.log(formData);
+
+        ev.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        formData = {};
+    }
+    function populareForm() {
+        const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+        if (savedData.email) {
+            inputEl.value = savedData.email;
+            formData.email = savedData.email;
+        }
+
+        if (savedData.message) {
+            textareaEl.value = savedData.message;
+            formData.message = savedData.message;
+        }
+    }
+}
