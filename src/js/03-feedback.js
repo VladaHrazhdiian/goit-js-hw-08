240d3582af1b3bfa2a7b137acf762f444e6212d9
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-06-18 18:28:06 +0300

    work

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const inputEl = document.querySelector('.feedback-form input');
-const textareaEl = document.querySelector('feedback-form textarea');
-const formEl = document.querySelector('feedback-form');
+const textareaEl = document.querySelector('.feedback-form textarea');
+const formEl = document.querySelector('.feedback-form');
 
 const STORAGE_KEY = 'feedback-form-state';
 
@@ -11,31 +11,36 @@ let formData = {};
 formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
 
-populareForm = {};
+populateForm();
 
 function onFormSubmit(ev) {
-    ev.preventDefaunt();
+  ev.preventDefault();
 
-    if (textareaEl.value === '' || inputEl.value === '') {
-        return alert('Заповніть всі поля');
+  if (textareaEl.value === '' || inputEl.value === '') {
+    return alert('Заповніть всі поля');
+  }
 
-        console.log(formData);
+  console.log(formData);
 
-        ev.currentTarget.reset();
-        localStorage.removeItem(STORAGE_KEY);
-        formData = {};
-    }
-    function populareForm() {
-        const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  ev.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+function onFormInput(ev) {
+  formData[ev.target.name] = ev.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
-        if (savedData.email) {
-            inputEl.value = savedData.email;
-            formData.email = savedData.email;
-        }
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-        if (savedData.message) {
-            textareaEl.value = savedData.message;
-            formData.message = savedData.message;
-        }
-    }
+  if (savedData.email) {
+    inputEl.value = savedData.email;
+    formData.email = savedData.email;
+  }
+
+  if (savedData.message) {
+    textareaEl.value = savedData.message;
+    formData.message = savedData.message;
+  }
 }
